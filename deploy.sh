eae9a23051953a1aa36e72d81a72068f165e7bf0
Mahmud Ridwan <m@hjr265.me>
2021-01-14 09:54:56 +0600

    Switch to Hugo Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-hugo && \
-(cd public/ && s3cmd sync --delete-removed --acl-public --exclude sitemap.xml . s3://hjr265.me/)
+set -e
+
+hugo deploy -v
+s3cmd setacl -rP s3://hjr265.me/
