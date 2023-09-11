23ff557d4a2afe10ba03a55cc1c595a6fcd4f917
Mahmud Ridwan <m@hjr265.me>
2023-09-11 16:10:24 +0600

    Remove deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-rm -rf public
-hugo --minify
-hugo deploy --maxDeletes=5
-s3cmd --access_key=$AWS_ACCESS_KEY_ID --secret_key=$AWS_SECRET_ACCESS_KEY setacl -rP s3://hjr265.me/
